53e3119ab7422f5d33936e021ceb5b3cab32062f
ZX10Tomcat <tomcat_iv@yahoo.com>
2017-07-28 11:21:05 +0200

    Basics

--- F1StatAndPred/Startup.cs
+++ F1StatAndPred/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using F1StatAndPred.DTO;
 
 namespace F1StatAndPred
 {
